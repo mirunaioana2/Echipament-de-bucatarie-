272b413fcabecbe2ba69ed4250f7b4f03ee13bc8
PricopiJustina107 <justinapricopi1@gmail.com>
2024-11-19 11:41:24 +0200

    EchipamentdeBucatarie.java

--- EchipamentdeBucatarie.java
+++ EchipamentdeBucatarie.java
@@ -2,8 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package com.mycompany.echipamentdebucatarie;
-
+package com.mycompany.echipamentdebucatarie
 /**
  *
  * @author Gigabyte
@@ -11,20 +10,20 @@ package com.mycompany.echipamentdebucatarie;
 //clasa parinte
 public class EchipamentdeBucatarie {
     protected String marca;
-    protected double pret;
+    protected float pret;
     protected double greutate;
 
 //constructor implicit
     public EchipamentdeBucatarie() {
         this.marca = "Necunoscuta";
-        this.pret = 0.0;
+        this.pret = 0.0f;
         this.greutate = 0.0;
     }
 
 //constructor cu parametri
     public EchipamentdeBucatarie(String marca, double pret, double greutate) {
         this.marca = marca;
-        this.pret = pret;
+        this.pret = (float) pret;
         this.greutate = greutate;
     }
 
