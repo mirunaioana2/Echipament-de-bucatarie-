068b2b3a981ef4cb4c7db89bfad42a5173c81230
Gabriela Stratulat <gstratulat10@yahoo.com>
2024-11-26 00:43:45 +0200

    Update TestEchipamente.java

--- TestEchipamente.java
+++ TestEchipamente.java
@@ -32,5 +32,33 @@ public class TestEchipamente {
         System.out.println(lada1);
         System.out.println(lada2);
         System.out.println(lada3);
+        
+        //  vector pentru Congelator
+        Object[] congelatoare = new Object[10];
+        for (int i = 0; i < congelatoare.length; i++) {
+            congelatoare[i] = new Congelator("Brand" + i, "Model" + i, 200 + i * 10, 150 + i * 5, "Electric", 2 + i, 
+                                             1500.0 + i * 100, "60x60x" + (120 + i), "Alb", 42 + i, true);
+        }
+
+        // lista pentru LadaFrigorifica
+        ArrayList<Object> laziFrigorifice = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            laziFrigorifice.add(new LadaFrigorifica("BrandLada" + i, "ModelLada" + i, 300 + i * 20, 180 + i * 10, "Electric", 3 + i, 
+                                                    2000.0 + i * 150, "70x70x" + (140 + i), "Argintiu", 50 + i, false));
+        }
+
+        // afisare elemente din vectorul de congelatoare
+        System.out.println("\nVector Congelatoare:");
+        for (Object obj : congelatoare) {
+            System.out.println(obj);
+        }
+
+        // afisare elemente din lista de lazi frigorifice
+        System.out.println("\nLista Lazi Frigorifice:");
+        for (Object obj : laziFrigorifice) {
+            System.out.println(obj);
+        }
     }
 }
+    
+
