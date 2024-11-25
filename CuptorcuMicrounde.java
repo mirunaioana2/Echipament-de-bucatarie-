f62b7931194f9271b123d4a3db4a456da8f59513
amaliapavel <amaliapavel74@gmail.com>
2024-11-25 21:32:41 +0200

    CuptorcuMicrounde.java

--- CuptorcuMicrounde.java
+++ CuptorcuMicrounde.java
@@ -8,41 +8,89 @@ package ro.ugal.aciee.echipamentdebucatarie;
  *
  * @author Amalia
  */
-// clasa CuptorcuMicrounde
-class CuptorcuMicrounde extends EchipamentdeBucatarie {
-    private int putere; 
-    private boolean areGrill;
+public class CuptorcuMicrounde extends echipamentdebucatarie {
+    private int putere;
+    private int capacitate;
+    private boolean grill;
+    private String culoare;
 
     // constructor fara argumente
     public CuptorcuMicrounde() {
         super();
         this.putere = 0;
-        this.areGrill = false;
+        this.capacitate = 0;
+        this.grill = false;
+        this.culoare = "";    
     }
 
-    // constructor cu toate argumentele
-    public CuptorcuMicrounde(String producator, double pret, String culoare, int putere, boolean areGrill) {
-        super(producator, pret, culoare);
+    // constructor cu toti parametrii
+    public CuptorcuMicrounde(String marca, String model, double pret, int garantie,
+                             String tipControl, int putere, int capacitate, boolean grill, String culoare) {
+        super(marca, model, pret, garantie, tipControl);
         this.putere = putere;
-        this.areGrill = areGrill;
+        this.capacitate = capacitate;
+        this.grill = grill;
+        this.culoare = culoare;
     }
 
     // constructor de copiere
-    public CuptorcuMicrounde(CuptorcuMicrounde other) {
-        super(other);
-        this.putere = other.putere;
-        this.areGrill = other.areGrill;
+    public CuptorcuMicrounde(CuptorcuMicrounde altCuptor) {
+        super(altCuptor);
+        this.putere = altCuptor.putere;
+        this.capacitate = altCuptor.capacitate;
+        this.grill = altCuptor.grill;
+        this.culoare = altCuptor.culoare;
     }
 
-    // rescriere metoda toString
+    
+    public int getPutere() {
+        return putere;
+    }
+
+    public void setPutere(int putere) {
+        this.putere = putere;
+    }
+
+    public int getCapacitate() {
+        return capacitate;
+    }
+
+    public void setCapacitate(int capacitate) {
+        this.capacitate = capacitate;
+    }
+
+    public boolean isGrill() {
+        return grill;
+    }
+
+    public void setGrill(boolean grill) {
+        this.grill = grill;
+    }
+
+    public String getCuloare() {
+        return culoare;
+    }
+
+    public void setCuloare(String culoare) {
+        this.culoare = culoare;
+    }
+    
+    
+    // implementarea metodei abstracte
+    @Override
+    public String tipEchipament() {
+        return "Cuptor cu microunde";
+    }
+
+    @Override
+    public double calculeazaCostTotal(double costLivrare) {
+        return getPret() + costLivrare;
+    }
+
+     // suprascrierea metodei toString
     @Override
     public String toString() {
-        return "CuptorCuMicrounde{" +
-                "producator='" + producator + '\'' +
-                ", pret=" + pret +
-                ", culoare='" + culoare + '\'' +
-                ", putere=" + putere +
-                ", areGrill=" + areGrill +
-                '}';
+        return super.toString() + ", Putere: " + putere + "W, Capacitate: " + capacitate + "L, Grill: " + grill + ", Culoare: " + culoare;
     }
+
 }
