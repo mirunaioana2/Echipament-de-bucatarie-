1815fb784224585dd35b5b31a915fa0d2f5e4b8b
PricopiJustina107 <justinapricopi1@gmail.com>
2024-11-25 19:24:44 +0200

    EchipamentdeBucatarie.java

--- EchipamentdeBucatarie.java
+++ EchipamentdeBucatarie.java
@@ -8,33 +8,93 @@ package com.mycompany.echipamentdebucatarie
  * @author Gigabyte
  */
 //clasa parinte
-public class EchipamentdeBucatarie {
-    protected String marca;
-    protected float pret;
-    protected double greutate;
-
-//constructor implicit
+abstract class EchipamentdeBucatarie {
+    private String marca;
+    private String model;
+    private double pret;
+    private int garantie; 
+    private String categorie; 
+    
+    //constructor fără argumente
     public EchipamentdeBucatarie() {
-        this.marca = "Necunoscuta";
-        this.pret = 0.0f;
-        this.greutate = 0.0;
+        this.marca = "";
+        this.model = "";
+        this.pret = 0.0;
+        this.garantie = 0;
+        this.categorie = "";
     }
 
-//constructor cu parametri
-    public EchipamentdeBucatarie(String marca, double pret, double greutate) {
+    //constructor cu toți parametrii
+    public EchipamentdeBucatarie(String marca, String model, double pret, int garantie, String categorie) {
         this.marca = marca;
-        this.pret = (float) pret;
-        this.greutate = greutate;
+        this.model = model;
+        this.pret = pret;
+        this.garantie = garantie;
+        this.categorie = categorie;
     }
 
-//metoda toString
+    // Constructor de copiere
+    public EchipamentdeBucatarie(EchipamentdeBucatarie other) {
+        this.marca = other.marca;
+        this.model = other.model;
+        this.pret = other.pret;
+        this.garantie = other.garantie;
+        this.categorie = other.categorie;
+    }
+
+    //metode abstracte
+    public abstract String descriereEchipament();
+    public abstract double calculeazaReducere(double procent);
+
+    //Getters și Setters
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public double getPret() {
+        return pret;
+    }
+
+    public void setPret(double pret) {
+        this.pret = pret;
+    }
+
+    public int getGarantie() {
+        return garantie;
+    }
+
+    public void setGarantie(int garantie) {
+        this.garantie = garantie;
+    }
+
+    public String getCategorie() {
+        return categorie;
+    }
+
+    public void setCategorie(String categorie) {
+        this.categorie = categorie;
+    }
+
+    //metoda toString
     @Override
     public String toString() {
-        return "EchipamentdeBucatarie{" +
-               "marca='" + marca + '\'' +
-               ", pret=" + pret +
-               ", greutate=" + greutate +
-               '}';
+        return "EchipamentDeBucatarie{" +"marca='" + marca + '\'' +", model='" + model + '\'' +", pret=" + pret +", garantie="
++ garantie +", categorie='" + categorie + '\'' +'}';
     }
 }
+
+
+
 
