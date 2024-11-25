6c2343b8019e28dc1a45180d5c29cd1493a94855
PricopiJustina107 <justinapricopi1@gmail.com>
2024-11-25 19:32:05 +0200

    ExpressorPJE.java

--- Expressor.java
+++ Expressor.java
@@ -9,26 +9,106 @@ package com.mycompany.echipamentdebucatarie;
  * @author Gigabyte
  */
 public class Expressor extends EchipamentdeBucatarie {
-    private double capacitateRezervor; 
-    private int putere; 
+    private double capacitateRezervor; // Capacitatea rezervorului de apă
+    private int putere; // Puterea în wați
+    private boolean rasnitaIncorporata; // Dacă are râșniță încorporată
+    private String tipCafea; // Tipul de cafea suportat (boabe, măcinată etc.)
+    private String culoare; // Culoarea expressorului
 
-//constructor implicit
+    // Constructor fără argumente
     public Expressor() {
-        super(); //apelează constructorul implicit al clasei părinte
-        capacitateRezervor = 0.0;
-        putere = 0;
+        super();
+        this.capacitateRezervor = 0.0;
+        this.putere = 0;
+        this.rasnitaIncorporata = false;
+        this.tipCafea = "";
+        this.culoare = "";
     }
 
-//constructor cu parametri
-    public Expressor(String marca, double pret, double greutate, double capacitateRezervor, int putere) {
-        super(marca, pret, greutate); // Apelează constructorul clasei părinte
+    // Constructor cu toți parametrii
+    public Expressor(String marca, String model, double pret, int garantie, String categorie, 
+                     double capacitateRezervor, int putere, boolean rasnitaIncorporata, String tipCafea, String culoare) {
+        super(marca, model, pret, garantie, categorie);
         this.capacitateRezervor = capacitateRezervor;
+        this.putere = putere;
+        this.rasnitaIncorporata = rasnitaIncorporata;
+        this.tipCafea = tipCafea;
+        this.culoare = culoare;
+    }
+
+    // Constructor de copiere
+    public Expressor(Expressor other) {
+        super(other);
+        this.capacitateRezervor = other.capacitateRezervor;
+        this.putere = other.putere;
+        this.rasnitaIncorporata = other.rasnitaIncorporata;
+        this.tipCafea = other.tipCafea;
+        this.culoare = other.culoare;
+    }
+
+    // Implementare metodă abstractă: descriereEchipament
+    @Override
+    public String descriereEchipament() {
+        return "Expressor " + getMarca() + " model " + getModel() + ", putere de " + putere + " W, cu capacitate rezervor " + capacitateRezervor + " litri.";
+    }
+
+    // Implementare metodă abstractă: calculeazaReducere
+    @Override
+    public double calculeazaReducere(double procent) {
+        return getPret() - (getPret() * procent / 100);
+    }
+
+    // Metodă toString
+    @Override
+    public String toString() {
+        return super.toString() +
+               ", Expressor{" +
+               "capacitateRezervor=" + capacitateRezervor +
+               ", putere=" + putere +
+               ", rasnitaIncorporata=" + rasnitaIncorporata +
+               ", tipCafea='" + tipCafea + '\'' +
+               ", culoare='" + culoare + '\'' +
+               '}';
+    }
+
+    // Getters și Setters
+    public double getCapacitateRezervor() {
+        return capacitateRezervor;
+    }
+
+    public void setCapacitateRezervor(double capacitateRezervor) {
+        this.capacitateRezervor = capacitateRezervor;
+    }
+
+    public int getPutere() {
+        return putere;
+    }
+
+    public void setPutere(int putere) {
         this.putere = putere;
     }
 
-//metodă pentru afișare
-    @Override
-    public String toString() {
-        return "Expressor: marca=" + marca + ", pret=" + pret +", greutate=" + greutate + ", capacitate=" + capacitateRezervor +" litri, putere=" + putere + "W";
+    public boolean isRasnitaIncorporata() {
+        return rasnitaIncorporata;
+    }
+
+    public void setRasnitaIncorporata(boolean rasnitaIncorporata) {
+        this.rasnitaIncorporata = rasnitaIncorporata;
+    }
+
+    public String getTipCafea() {
+        return tipCafea;
+    }
+
+    public void setTipCafea(String tipCafea) {
+        this.tipCafea = tipCafea;
+    }
+
+    public String getCuloare() {
+        return culoare;
+    }
+
+    public void setCuloare(String culoare) {
+        this.culoare = culoare;
     }
 }
