207a8ff0958894c9ebde8d0ad34b5082637db579
PricopiJustina107 <justinapricopi1@gmail.com>
2024-11-28 10:32:34 +0200

    TestPJE.java

--- TestPJE.java
+++ TestPJE.java
@@ -44,10 +44,12 @@ public class Test {
 
         // Vector pentru Cafetiera
         ArrayList<Object> cafetiere = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 20; i++) {
             cafetiere.add(new Cafetiera("Cafetiera" + i, "ModelC" + i, 200.0 + i * 20, 18,
                     "Manual", 1.0 + i * 0.2, "Plastic", false, 10, "Filtru permanent"));
         }
+        if(i % 2 == 0)
+            System.out.println("ModelC este par:" + cafetiere.get(i));}
 
         // Afisare exemple
         System.out.println("\nExpressors:");
